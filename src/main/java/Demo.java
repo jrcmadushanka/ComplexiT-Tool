b8bc0637b9a6dd8132baa9336b7cbc158baf68b2
chathura <jrcmadushanka@gmail.com>
2019-08-11 01:30:58 +0530

    Fixed character finding issue

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,4 +1,8 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Demo {
@@ -15,16 +19,15 @@ public class Demo {
         int line = 0;
         String lineContent = "";
 
-        while (true) {
+        while (scan.hasNext()) {
 
             lineContent = scan.nextLine();
 
             if (!lineContent.isEmpty()) {
                 code.add(lineContent);
 
-                System.out.println("test::test".contains("::"));
-                System.out.println("------------------------------------------------------------------------");
-                System.out.println(line++ + " " + lineContent.trim());
+                System.out.println( "===========================================================================" );
+                System.out.println(line++ + " " + lineContent.trim()); //8
 
                 tokens = Demo.complexityDueToSize(lineContent);
 
@@ -40,41 +43,93 @@ public class Demo {
             }
         }
 
+        scan.close();
+
     }
 
     private static ArrayList complexityDueToSize(String statement) {
 
         int complexity = 0;
         String[] words = statement.split(" ");
+        List<String> wordList = new ArrayList<>(Arrays.asList(words));
 
-        String[] args = {"+", "-", "*", "/", "%", "++", "--", "==", "!=", ">", "<", ">=", "<=", "&&", "||", "!", "|",
-                "^", "~", ">>", "<<", ">>>", "<<<", "+=", "-=", "*=", "/=", "=", ">>>=", "|=", "&=", "%=", "<<=", ">>=", "^=",
-                ",", "->", ".", "::", "void", "double", "int", "float", "string", "printf",  "println", "count", "cin", "if", "for",
-                "while", "do-while", "switch", "case", "String", "Class", "method", "object", "variable", "class", "import",
-                "endl",  "\n", "\t"};
+        String[] strings = statement.split("\"");
+
+        String[] args = {"- ", "*", "/ ", "% ", "++", "--", "== ", "!=", " > ", " < ", ">=", "<=", "&&", "||", "!", "| ",
+                "^ ", "~ ", ">> ", "<< ", ">>>", "<<<", "+=", "-=", "*=", "/=", "= ", ">>>=", "|=", "&=", "%=", "<<=", ">>=", "^=",
+                ",", "->", "::", "void ", "double ", "int ", "float ", "string ", "printf",  "println", "count",
+                " cin", "if ", "if(", "for ","while ", "do-while","do{","do {", "switch ","switch (", "case ",
+                "String", "Class", "method", "object", "variable", "class", "import","default:","default ", "endl",  "\\n", "\\t"};
+        String[] argLevel2 = {".", "+"};
+
+        List<String> level2Args = Arrays.asList(argLevel2);
 
         ArrayList<String> tokens = new ArrayList();
 
         tokens.add(0, Integer.toString(complexity));
+
         for (String word : words
         ) {
-
             if (word.startsWith("\"") && word.endsWith("\"")){
-                if (!tokens.contains(word)) {
-                    tokens.add("\"" + word + "\"");
-                }
-                complexity++;
                 continue;
             }
+
+            word = word + " ";
+
             for (String arg: args
                  ) {
                 if (word.contains(arg)){
-                    if (!tokens.contains(arg)) {
+
+                   // if (!tokens.contains(arg)) {
                         tokens.add(arg);
-                    }
+                        if(!level2Args.contains(word.trim())){
+                            wordList.remove(word.trim());
+                            wordList.add(word.trim().replace(arg,""));
+                            System.out.println(word.trim());
+                            System.out.println(wordList.indexOf(arg.trim()));
+                            System.out.println(wordList.get(wordList.indexOf(word.trim())));
+                        }
+                   // }
                     complexity++;
                 }
             }
+
+        }
+
+        for (String word: wordList
+             ) {
+            System.out.println(word);
+            for (int i = 0; i < word.length() ; i++) {
+
+                if (String.valueOf(word.charAt(i)).equals(".")) {
+
+                    // if (!tokens.contains(".")) {
+                    tokens.add(".");
+                    //  }
+                    complexity++;
+                }
+                if (String.valueOf(word.charAt(i)).equals("+")) {
+
+                    // if (!tokens.contains(".")) {
+                    tokens.add("+");
+                    //  }
+                    complexity++;
+                }
+            }
+        }
+
+        int stringCount = 0;
+
+        for (String s: strings
+        ) {
+            stringCount++;
+            if (stringCount % 2 == 0){
+                if (!tokens.contains(s)) {
+                    tokens.add(s);
+                }
+                complexity++;
+            }
+            continue;
         }
 
         tokens.set(0, Integer.toString(complexity));
